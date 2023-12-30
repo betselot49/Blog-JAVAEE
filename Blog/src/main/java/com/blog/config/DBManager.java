cf6d51f12069a665a66f942358070594e7cd73bc
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2023-12-30 13:54:34 +0300

    Model Finished

---
+++ Blog/src/main/java/com/blog/config/DBManager.java
@@ -0,0 +1,65 @@
+package com.blog.config;
+
+
+import com.blog.models.*;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+import java.sql.*;
+
+
+public class DBManager implements ServletContextListener {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/blogapp";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "241128";
+
+    private static Connection instance = (new DBManager()).getConnection();
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load JDBC driver. Make sure the driver JAR is in the classpath.", e);
+        }
+    }
+
+
+    public static Connection getInstance() {
+        return instance;
+    }
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to create employee table.", e);
+        }
+    }
+    public void contextInitialized(ServletContextEvent sce) {
+        try  {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement user = connection.prepareStatement(User.schema());
+            PreparedStatement blog = connection.prepareStatement(Blog.schema());
+            PreparedStatement readingList = connection.prepareStatement(ReadingList.schema());
+            PreparedStatement like = connection.prepareStatement(Like.schema());
+            PreparedStatement comment = connection.prepareStatement(Comment.schema());
+            PreparedStatement save = connection.prepareStatement(Save.schema());
+            user.execute();
+            blog.execute();
+            readingList.execute();
+            like.execute();
+            comment.execute();
+            save.execute();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to create tables.", e);
+        }
+
+    }
+
+    public void contextDestroyed() throws SQLException {
+        instance.close();
+    }
+}
--- Blog/src/main/java/com/blog/models/Blog.java
+++ Blog/src/main/java/com/blog/models/Blog.java
@@ -1,13 +1,153 @@
 package com.blog.models;
 
+import com.blog.config.DBManager;
+
 import java.lang.reflect.Array;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Blog {
     public int Id;
     public String Content;
     public int UserId;
 
-    public Array Tags;
-    public int LikeCount;
-    public int CommentCount;
+    public String[] Tags = {};
+    public int LikeCount = 0;
+    public int CommentCount = 0;
+
+    public Date CreatedAt;
+
+    private static Connection connection = DBManager.getInstance();
+
+
+    public static String schema(){
+        String query = "CREATE TABLE IF NOT EXIST blogs (" +
+                "Id INT AUTO_INCREMENT PRIMARY KEY," +
+                "Content TEXT," +
+                "UserId INT," +
+                "Tags VARCHAR(255)," +
+                "LikeCount INT," +
+                "CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+                "CommentCount INT," +
+                "FOREIGN KEY (UserId) REFERENCES users(UserId) ON DELETE CASCADE" +
+                ");";
+        return query;
+    }
+
+
+
+    public static Blog build(ResultSet result) throws Exception {
+        Blog blog = new Blog();
+        blog.Id = result.getInt("Id");
+        blog.Content = result.getString("Content");
+        blog.UserId = result.getInt("UserId");
+        blog.LikeCount = result.getInt("LikeCount");
+        blog.CommentCount = result.getInt("CommentCount");
+        blog.CreatedAt = result.getDate("CreatedAt");
+        blog.Tags = result.getString("Tags").split(",");
+
+        return blog;
+    }
+
+
+    public void create() throws Exception {
+        String query = "INSERT INTO blogs(Content, UserId, Tags, LikeCount, CommentCount) VALUES(?,?,?,?,?)";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, this.Content);
+        stmt.setInt(2, this.UserId);
+        stmt.setString(3, String.join(",", this.Tags));
+        stmt.setInt(4, this.LikeCount);
+        stmt.setInt(5, this.CommentCount);
+        stmt.executeUpdate(query);
+        stmt.close();
+    }
+
+
+    public void update() throws Exception {
+        String query = "UPDATE blogs SET Content=?, Tags=? WHERE Id=?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, this.Content);
+        stmt.setString(2, String.join(",", this.Tags));
+        stmt.executeUpdate(query);
+        stmt.close();
+    }
+
+    public void delete() throws Exception {
+        String query = "DELETE FROM blogs WHERE Id=?";
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(query);
+        stmt.close();
+    }
+
+    public static Blog getById(int id) throws Exception {
+        String query = "SELECT * FROM blogs WHERE Id=?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setInt(1, id);
+        var result = stmt.executeQuery();
+        Blog blog = null;
+        if (result.next()) {
+            return Blog.build(result);
+        }
+        stmt.close();
+        return null;
+    }
+
+    public static ArrayList<Blog> getByUserId(int userId) throws Exception {
+        String query = "SELECT * FROM blogs WHERE UserId=?";
+        Statement stmt = connection.createStatement();
+        var result = stmt.executeQuery(query);
+        ArrayList<Blog> blogs = new ArrayList<>();
+        while (result.next()) {
+            blogs.add(Blog.build(result));
+        }
+        stmt.close();
+        return blogs;
+    }
+
+
+    public static ArrayList<Blog> getAll() throws Exception {
+        String query = "SELECT * FROM blogs";
+        Statement stmt = connection.createStatement();
+        var result = stmt.executeQuery(query);
+        ArrayList<Blog> blogs = new ArrayList<>();
+        while (result.next()) {
+            blogs.add(Blog.build(result));
+        }
+        stmt.close();
+        return blogs;
+    }
+
+    public static ArrayList<Blog> search(String param) throws Exception {
+        String query = "SELECT * FROM blogs WHERE Content LIKE ? OR Tags LIKE ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, "%" + param + "%");
+        stmt.setString(2, "%" + param + "%");
+        var result = stmt.executeQuery();
+        ArrayList<Blog> blogs = new ArrayList<>();
+        while (result.next()) {
+            blogs.add(Blog.build(result));
+        }
+        stmt.close();
+        return blogs;
+    }
+
+    public static ArrayList<Blog> getByTag(String tag) throws Exception {
+        String query = "SELECT * FROM blogs WHERE Tags LIKE ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1, "%" + tag + "%");
+        var result = stmt.executeQuery();
+        ArrayList<Blog> blogs = new ArrayList<>();
+        while (result.next()) {
+            blogs.add(Blog.build(result));
+        }
+        stmt.close();
+        return blogs;
+    }
+
+
+
 }
