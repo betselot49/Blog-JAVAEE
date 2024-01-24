50a9ae304cabae8cc404ea66ce57b234082d4a31
fikreanteneh <fikremariam.anteneh@aait.edu.et>
2024-01-24 23:29:42 +0300

    Bug Fix

--- BlogApp/src/main/java/com/blog/config/DBManager.java
+++ BlogApp/src/main/java/com/blog/config/DBManager.java
@@ -13,8 +13,6 @@ public class DBManager {
 	private static final String URL = "jdbc:mysql://localhost:3306/blogapp";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "241128";
-//	private static final String PASSWORD = "mysql";
-
 	public static Connection connection;
 
     static {
